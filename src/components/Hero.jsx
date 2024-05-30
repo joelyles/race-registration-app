cdc9656ad09156510e49f2bbad8ce622fe34b7c6
Joe Lyles <lylesjoe@protonmail.com>
2024-05-30 05:29:53 -0400

    updated backdrop width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ const Hero = () => {
     <>
       <section style={{ backgroundImage: `url(public/images/martins-zemlickis-NPFu4GfFZ7E-unsplash.jpg)`
        }} className="bg-fixed bg-cover bg-center" >
-        <div className="backdrop-brightness-35 mx-auto max-w-6xl pt-10 px-2 sm:px-6 md:px-8">
-          <div className="flex flex-col items-center justify-between sm:m-auto h-36 sm:h-36 md:h-48">
+        <div className="backdrop-brightness-35 mx-auto pt-10 px-2 sm:px-6 md:px-8">
+          <div className="flex flex-col items-center justify-between max-w-6xl sm:m-auto h-36 sm:h-36 md:h-48">
             <div className="flex flex-col items-center">
               <div className="text-4xl font-semibold md:text-5xl text-slate-200">Macon Marathon</div>
               <div className="hidden md:block tracking-wider text-slate-200">nothing special, just a marathon in macon, ga</div>
