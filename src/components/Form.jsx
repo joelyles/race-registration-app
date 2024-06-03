63cc43c08de0d421986da72dc866a49ee18fa48a
Joe Lyles <lylesjoe@protonmail.com>
2024-06-03 17:16:25 -0400

    width adjustment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,10 +4,10 @@ const Form = () => {
   return (
     <>
       <div className="h-full flex px-4 bg-slate-200 ">
-        <div className="flex flex-col mx-auto mt-8 mb-8 p-6 rounded-md shadow-md bg-white h-3/4 w-3/4">
+        <div className="flex flex-col mx-auto mt-8 mb-8 p-6 rounded-md shadow-md bg-white h-3/4 w-vw sm:w-3/4">
           <h4 className="flex font-semibold tracking-wider justify-center text-2xl ">Registration Form</h4>
           <div className="flex flex-col items-center p-8 bg-blue-200 mt-6 rounded-md shadow-md h-3/4">
-            <div className="flex flex-col overflow-scroll my-auto bg-blue-100 max-w-m w-3/4 p-8 rounded-md">
+            <div className="flex flex-col overflow-scroll my-auto bg-blue-100 max-w-m w-vw p-8 rounded-md">
               <div className="flex flex-col justify-left gap-4 p-2">
                 <div className="flex flex-col grow">
                   <label className="p-1">First Name</label>
