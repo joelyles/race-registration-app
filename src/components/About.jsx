86cdd74e0e193b9a95d2e75d8a22cfd622899e61
Joe Lyles <lylesjoe@protonmail.com>
2024-07-15 05:56:07 -0400

    style and ui updates to home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,14 @@
 import RegisterButton from "./RegisterButton"
+import { Link } from "react-router-dom"
 
 const About = () => {
   return (
     <div className="flex h-auto bg-slate-300 border-t-slate-700">
       <div className="flex flex-col items-left py-10 px-12 mx-auto max-w-7xl">
-       <p className="tracking-wider pb-10">This is probably the hottest time of year to run a marathon in Georgia. But thats ok, we will have plenty of hydration and nutrition for you. There will be aid stations with tents that have mist machines to cool you off. We will start at 5am, so that it's a little cooler out. This will be a point to point race and you will finish up by running into the north end-zone of the Mercer Bears football stadium. Check out our testimonials. Sign up today.</p>
-       <RegisterButton />
+       <p className="tracking-wider pb-10">This is probably the hottest time of year to run a marathon in Georgia. But thats ok, we will have plenty of hydration and nutrition for you. There will be aid stations with tents that have mist machines to cool you off. We will start at 5am, so that it's a little cooler out. This will be a point to point race and you will finish up by running into the north end-zone of the Mercer Bears football stadium. Check out our testimonials.</p>
+       <div className="flex">
+       <Link to="/register" className="flex mx-auto max-w-32 text-center bg-slate-500 text-slate-200 px-4 py-2 rounded-md hover:bg-blue-900">Register Now!</Link>
+       </div>
       </div>
     </div>
   )
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ const Cards = () => {
   const reviews = testimonials;
 
   return (
-    <section className="py-12 px-12 mx-auto max-w-7xl">
+    <section className="py-12 px-12 mx-auto mb-56 max-w-7xl">
       <div className="m-auto">
         <h4 className="text-2xl text-center">Testimonials</h4>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 py-4">
